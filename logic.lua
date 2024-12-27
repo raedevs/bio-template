eea866e5fe4a282f5a04be291d78339e416af35b
rae <raesocialscontact@gmail.com>
2024-12-27 04:29:57 +0000

    Create logic.lua

---
+++ logic.lua
@@ -0,0 +1,4 @@
+-- Lua script to generate dynamic content
+-- Example: Returning a short bio
+local bio = "I am a developer passionate about creating seamless web experiences and learning new technologies."
+print(bio)
